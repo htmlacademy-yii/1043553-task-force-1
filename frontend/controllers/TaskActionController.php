13e54aefb696e263269bf9e5edb0934d246413a7
Ar0010r <ar0010r@gmail.com>
2020-09-08 12:47:47 +0300

    делаю отклик на задание

---
+++ frontend/controllers/TaskActionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace frontend\controllers;
+
+use frontend\controllers\parentControllers\SecuredController;
+use frontend\models\forms\TaskCompleteForm;
+use frontend\models\forms\TaskCreateForm;
+use frontend\models\forms\TaskResponseForm;
+
+class TaskActionController extends SecuredController
+{
+    public function actionRespond()
+    {
+        
+    }
+
+    public function actionAccomplish()
+    {
+        
+    }
+
+    public function actionRefuse()
+    {
+        
+    }
+
+    public function actionCancel()
+    {
+        
+    }
+}
